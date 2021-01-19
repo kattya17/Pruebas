aa032c7f35f6a6274cad066b1f497367427cb7a7
Katty171 <ktorrezy@fcpn.edu.bo>
2021-01-19 10:16:55 -0400

    mi primer commit

---
+++ codigo.js
@@ -0,0 +1,79 @@
+//variables
+recipiente = "papel";
+//Tipos de datos
+// Cadena de texto
+String = "Kattya es una cadena de texto";
+//Numeros
+number = 82;
+//booleano 0 y 1 true false
+booleano= false;
+//undefined, null y nan variable no definida o existe un error
+// formas de declarar variable var, let, const
+
+var numero = 18;
+let num=20; //limita es mas usado let para numeros
+const nom = "constante" //no puede cambiar su valor 
+
+
+
+//alert(recipiente);
+//funcion prompt para recibir informacion
+
+let nombre = prompt("Dime tu nombre");
+
+alert("Hola"+ nombre);
+
+//escribe en el documento
+document.write(nombre);
+//Operadores de asignacion
+let numeros = 6;
+numeros +=5;
+numeros -=2;
+numeros %=3;
+
+document.write(numeros);
+
+//Operadores numericos/ aritmetico
+numeroq = 10;
+numero2 = 8
+resultado =numeroq +numero2;
+
+document.write(resultado);
+
+numero2--;
+
+document.write(numero2);
+//exponencial
+resultado= numeroq**2
+
+document.write(resultado);
+
+//concatenar strings otro concat es para strings
+
+saludo = "hola";
+pregunta ="que tal";
+
+frase = saludo + pregunta;
+
+document.write(frase);
+
+//concatenar numeros con " " cuando hay una cadena de texto convierte todo a texto
+ num1 = 4;
+ num2 = 6;
+
+ frase= " "+ num1 + num2; 
+ document.write(frase);
+
+ //concatenar con backtiks ${}
+
+ f1 = "jajajj";
+ f2= "ohoh" + nombre +" nip";
+ document.write (f2);
+
+ f3 = 'ohoh ${nombre} nip nip´';
+
+ document.write(f3);
+
+
+
+
